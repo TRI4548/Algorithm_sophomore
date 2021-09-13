172ceee22489ead444f1f693a145d81fa5afba45
Minh <ntri4548@gmail.com>
2021-09-13 21:30:53 +0700

    Add new c# console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithm_sophomore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
